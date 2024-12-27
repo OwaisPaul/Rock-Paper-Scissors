a882888df2c1e6a958fcda6a295bd25a5982d635
OwaisPaul <152506988+OwaisPaul@users.noreply.github.com>
2024-12-27 11:42:14 +0530

    final day

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
 
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"];
-    return choices.random()
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    return computerChoice.toLowerCase();
 }
 
-console.log(getComputerChoice())
+
+function getHumanChoice(){
+ let humanChoice = window.prompt("Enter the choice")
+ return humanChoice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice){
+  if(humanChoice === computerChoice){
+    return "It's a tie!"
+  } 
+
+  if( 
+    (humanChoice == "rock" && computerChoice == "scissors")||
+    (humanChoice == "paper" && computerChoice == "rock") ||
+    (humanChoice == "scissors" && computerChoice == "paper")
+  ){ 
+ return "you win"
+ humanScore++;
+} else{
+    return "you lose"
+    computerScore++;
+}
+}
+
+function playGame(){
+    for (let i = 0; i < 5; i++) {
+   const humanSelection = getHumanChoice();
+   const computerSelection = getComputerChoice();
+   playRound(humanSelection, computerSelection);
+}
+console.log(`Final Score: You - ${humanScore}, Computer - ${computerScore}`);
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You are the overall winner!");
+    } else if (computerScore > humanScore) {
+        console.log("Sorry! The computer is the overall winner.");
+    } else {
+        console.log("It's an overall tie!");
+    }
+}
+console.log(playGame());
